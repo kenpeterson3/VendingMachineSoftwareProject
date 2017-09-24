ac4fc0bd19d0cecd835705eeae9dd9c67629ae1b
Ken Peterson <kenpeterson3@gmail.com>
2017-09-24 11:16:29 -0400

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,70 @@
+package com.techelevator.view;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Menu {
+
+	private PrintWriter out;
+	private Scanner in;
+
+	public Menu(InputStream input, OutputStream output) {
+		this.out = new PrintWriter(output);
+		this.in = new Scanner(input);
+	}
+
+	public Object getChoiceFromOptions(Object[] options) {
+		Object choice = null;
+		while (choice == null) {
+			displayMenuOptions(options);
+			choice = getChoiceFromUserInput(options);
+		}
+		return choice;
+	}
+
+	public BigDecimal getDecimalFromUser(String message) {
+		BigDecimal inputtedDecimal = null;
+		out.println(message);
+		do {
+			try {
+				String userInput = in.nextLine();
+				inputtedDecimal = new BigDecimal(userInput);
+			} catch (NumberFormatException ex) {
+				out.print("Please input a valid number >>> ");
+				out.flush();
+			}
+		} while (inputtedDecimal == null);
+
+		return inputtedDecimal;
+	}
+
+	private Object getChoiceFromUserInput(Object[] options) {
+		Object choice = null;
+		String userInput = in.nextLine();
+		try {
+			int selectedOption = Integer.valueOf(userInput);
+			if (selectedOption > 0 && selectedOption <= options.length) {
+				choice = options[selectedOption - 1];
+			}
+		} catch (NumberFormatException e) {
+
+		}
+		if (choice == null) {
+			out.println("\n*** " + userInput + " is not a valid option ***\n");
+		}
+		return choice;
+	}
+
+	private void displayMenuOptions(Object[] options) {
+		out.println();
+		for (int i = 0; i < options.length; i++) {
+			int optionNum = i + 1;
+			out.println(optionNum + ") " + options[i]);
+		}
+		out.print("\nPlease choose an option >>> ");
+		out.flush();
+	}
+}
