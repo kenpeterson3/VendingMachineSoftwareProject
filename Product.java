188424a85b70237e665549212b1c2bcb53755dcd
Ken Peterson <kenpeterson3@gmail.com>
2017-10-01 22:03:25 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 
 public abstract class Product {
 
-	public String name;
-	public BigDecimal price;
+	private String name;
+	private BigDecimal price;
 
 	public Product(String name, BigDecimal price) {
 		this.name = name;
